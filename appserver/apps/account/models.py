ac984fffb951bc77a14efeda77f3080a2df63471
Kimsuncheol <cheonjae6@naver.com>
2025-12-03 16:45:31 +0900

    feat: Initialize project with `main.py` and virtual environment dependencies.

---
+++ appserver/apps/account/models.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+from sqlalchemy import UniqueConstraint
+from sqlalchemy_utc import UtcDateTime
+
+class User(SQLModel, table=True):
+    __table__ = "users"
+    __table_args__ = (
+        UniqueConstraint("email", name="uq_email")
+    )
+
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, max_length=40, description="user account ID")
+    email: EmailStr = Field(max_length=128, description="user email")
+    display_name: str = Field(max_length=40, description="user display name")
+    password: str = Field(max_length=128, description="user password")
+    is_host: bool = Field(default=False, description="Whether the user is a host")
+    created_at: datetime = Field(
+        default=None, 
+        nullable=False, 
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+        })
+    updated_at: datetime = Field(
+        default=None, 
+        nullable=False, 
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+            "onupdate": lambda: datetime.now(timezone.utc)
+        })
+
+class OAuthAccount(SQLModel, table=True):
+    __table__ = "oauth_accounts"
+    __table_args__ = (
+        UniqueConstraint(
+            "provider",
+            "provider_user_id",
+            name="uq_provider_provider_account_id",
+        ),
+    )
+
+    provider: str = Field(max_length=10, description="OAuth provider")
+    provider_account_id: str = Field(max_length=128, description="OAuth provider account ID")
+
+    user_id: int = Field(foreign_key="users.id")
+    user: USer = Relationship()
+
+    created_at: AwareDateTime = Field(
+        default=None, 
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+        },
+    )
+    updated_at: AwareDateTime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+            "onupdate": lambda: datetime.now(timezone.utc)
+        }
+    )
